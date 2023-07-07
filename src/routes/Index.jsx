dc1fa70d870a935c02c61c0f03b131d4a6dc0988
Robin <robin@MacBook-de-Robin.local>
2023-07-07 11:51:28 -0600

    english text changed to spanish and removed option for email

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -4,30 +4,13 @@ export default function Index() {
     return (
         <div className="row row-content">
             <div className="col">
-                <h1 className="brand-slogan">
-                    Emotional learning is important
-                </h1>
+                <h1 className="brand-slogan">Aprende felíz</h1>
                 <p className="brand-description">
-                    Recent studies have shown that including emotions during the
-                    learing process leads to better results. It is also shown
-                    that every human action is triggered by an emotion.
+                    Estudios recientes han demostrado que incluir las Emociones
+                    durante el proceso de aprendizaje, mejora los resultados.
+                    También está demostrado que cada acción humana es provocada
+                    por una emoción.
                 </p>
-                <div className="input-group mb-3">
-                    <input
-                        type="text"
-                        className="form-control custom-input"
-                        placeholder="Your email"
-                        aria-label="Your email"
-                        aria-describedby="button-addon2 custom-addon"
-                    ></input>
-                    <button
-                        className="btn btn-outline-secondary custom-btn"
-                        type="button"
-                        id="button-addon2"
-                    >
-                        Registrarse
-                    </button>
-                </div>
             </div>
             <div className="col">
                 <img src={Illustration1} />
